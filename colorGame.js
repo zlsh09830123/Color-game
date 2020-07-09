ee06ef6ab0feb4c9ed00c49d0ee7f9207f6ee355
Hans Li <zlsh09830123@gmail.com>
2020-07-09 13:05:22 +0800

    Add styling

--- colorGame.js
+++ colorGame.js
@@ -20,7 +20,7 @@ resetButton.addEventListener("click", function() {
     for(let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
 });
 
 easyBtn.addEventListener("click", function() {
